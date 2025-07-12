4b9e2a0e1050b36d6ed3fa582013504c9b47f313
akhil <akhilgowda21@gmail.com>
2025-07-12 10:19:38 +0000

    update main.c

--- main.c
+++ main.c
@@ -3,13 +3,22 @@
 void biggest();
 void factorial();
 void reverse();
- 
+void big();
+void palindrome();
+void fibonacci();
+void sum();
+void sort();
 
   int main() {
 
 	    biggest();
 	    factorial();
 	    reverse();
+	    big();
+	    palindrome();
+	    fibonacci();
+	    sum();
+	    sort();
 	    return 0;
 }
 	
